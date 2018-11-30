7721005afcf9355ff22cee1a578a354c215fa0bd
Zhensheng Wu <zhensheng.five@gmail.com>
2018-11-30 18:57:13 +0800

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -14,7 +14,7 @@ sudo pacman -S nmap ydcv tree vscode chromium privoxy zsh expect
 # sudo systemctl enable sshd
 
 # install oh my zsh
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh`
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 
 # --------------------------------------------------function------------------------------------------------
 function install_shadowsock(){
