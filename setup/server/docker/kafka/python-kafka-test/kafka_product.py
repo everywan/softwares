b4c96cd153b365f399b86ab80e430a4e995f9ce6
wzs <zhensheng.five@gmail.com>
2020-04-20 18:36:40 +0800

    更新kafka producer python example

--- setup/server/docker/kafka/python-kafka-test/kafka_product.py
+++ setup/server/docker/kafka/python-kafka-test/kafka_product.py
@@ -8,7 +8,7 @@ topic = "test_topic"
 bootstrap_servers=['127.0.0.1:9092']
 
 def produce():
-    producer = KafkaProducer(bootstrap_servers)
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
     # Asynchronous by default
     future = producer.send(topic, b'raw_bytes')
@@ -16,10 +16,9 @@ def produce():
     # Block for 'synchronous' sends
     try:
         record_metadata = future.get(timeout=10)
-    except KafkaError:
-        # Decide what to do if produce request failed...
-        log.exception()
-        pass
+    except KafkaError as e:
+        print("发送失败. KafkaError: ", repr(e))
+        return 
     
     # Successful result returns assigned partition and offset
     print ("发送成功: topic: %s, partition: %s, offset:%s" % (record_metadata.topic,
