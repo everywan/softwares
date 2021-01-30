252f00501df4c1a6f5ca1441655aa7b8fb2a9258
wzs <zhensheng.five@gmail.com>
2021-01-31 01:56:06 +0800

    fix

--- setup/centos-server/docker/redis/cli.sh
+++ setup/centos-server/docker/redis/cli.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # redis_cli, 通过docker链接
 docker run -it --network redis_default --rm redis redis-cli -h redis
+# AUTH password
--- setup/centos-server/setup.d/8-docker.sh
+++ setup/centos-server/setup.d/8-docker.sh
@@ -9,8 +9,7 @@ function install(){
     # 如果你的机器不能访问全球网, 不要执行下面这一步. 现在一般源仓库都有docker, 不去官方拉也行.
     # sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-    sudo yum install -y docker
-    sudo pip install docker-compose
+    sudo yum install -y docker docker-compose
 
     echo "配置docker免sudo. 注意: 需要重新登入终端, user 才可以使用docekr组的权限"
     sudo groupadd docker
