9ee0ab6b16863db04e582281b2329995decf8868
wzs <zhensheng.five@gmail.com>
2021-01-31 15:23:49 +0800

    fix config

--- setup/centos-server/setup.d/8-etc.sh
+++ setup/centos-server/setup.d/8-etc.sh
@@ -2,7 +2,10 @@
 
 function install(){
     echo "------------------------ 从源安装的一些软件 -------------------------\n"
-    sudo yum install -y nmap tree expect tig
+    sudo yum install -y nmap tree expect tig supervisor
+    
+    sudo systemctl start supervisord.service
+    sudo systemctl enable supervisord.service
 
     # notebook
     # [参考官方文档](https://linrunner.de/en/tlp/docs/tlp-configuration.html)
