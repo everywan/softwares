c1d4a871433cd906424cc50e2bb3f83eea891c8a
Zhensheng Wu <zhensheng.five@gmail.com>
2022-02-03 21:28:15 +0800

    Update 1-go.sh

--- setup/arch/install/script/1-go.sh
+++ setup/arch/install/script/1-go.sh
@@ -2,9 +2,7 @@
 
 function install(){
     echo "------------------------ 安装 go -------------------------\n"
-    wget -c https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz -O go.tar.gz
-    tar -xzf go.tar.gz && sudo mv go /usr/local/src && sudo ln -s /usr/local/src/go/bin/go /usr/local/bin/go
-    rm go.tar.gz
+    sudo pacman -S --noconfirm -q go go-tools
 }
 
 install
