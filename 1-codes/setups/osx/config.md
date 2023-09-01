9367ea65db51cabccddf6d695f66aae24e71a6ae
wzs <zhensheng.five@gmail.com>
2023-09-01 15:52:26 +0800

    [fix] update osx.setup

--- 1-codes/setups/osx/config.md
+++ 1-codes/setups/osx/config.md
@@ -1,4 +1,4 @@
-## OSX 初始化安装
+# OSX 初始化安装
 <!-- TOC -->
 - [OSX 初始化安装](#osx-初始化安装)
     - [脚本安装](#脚本安装)
@@ -8,63 +8,29 @@
     - [设置快捷键启动](#设置快捷键启动)
 <!-- /TOC -->
 
-### 脚本安装
+## 脚本安装
 执行以下命令即可， [脚本源码点这里](./config.sh)
 ```Bash
 curl -sSL https://github.com/everywan/soft/blob/master/os/osx/config.sh | sh -
 ```
 
-### 手动安装
+## 手动安装
 下载安装
 - [搜狗输入法-下载地址](https://pinyin.sogou.com/mac/)
-- [vscode-下载地址](https://code.visualstudio.com/)
-- [chrome-下载地址](https://www.google.com/chrome/) 
-- [vmware-下载地址](https://www.vmware.com/go/getfusion)
-    - [vmare配置](./vmare/vmare.md)
-- [Navicat Premium](http://xclient.info/s/navicat-premium.html?t=c0321e621d18b21e2ba8791a627b3f9bc45dd6a9)
-    - 下载完打开时, 会报错, 执行以下命令 `xattr -cr /Applications/Navicat\ Premium.app/`, 然后就可以正常打开了.
-- [ss配置教程(脚本已安装命令行ss)](/collect/soft/shadowsocks.md)
-- Alfred: 推荐, 但是我没有用. 更习惯使用 下拉终端替代Alfred的命令执行, cli版本的ydcv替代词典, 其他的功能对我用处不大.
+- [karabiner-elements](https://karabiner-elements.pqrs.org/). 改键工具
 
-商店安装
-- wechat
-- 网易云音乐
+## 配置
+### item2
+首次配置:
+. 字体大小修改为 14. `setting->profile->text`
+. 修改为半透明. `setting->profile->windows.transparency`
 
-### 配置恢复
-执行以下命令即可.
-```Bash
-# 备份
-curl -sSL https://github.com/everywan/soft/blob/master/os/osx/script/backup-restore.sh backup | sh -
-# 恢复
-curl -sSL https://github.com/everywan/soft/blob/master/os/osx/script/backup-restore.sh restore | sh -
-```
+配置一次后, 建议保存配置, 后续直接回复配置即可.
 
-#### iTerm2配置指南
-- [iTerm2配置-参考文章](http://huang-jerryc.com/2016/08/11/打造高效个性Terminal（一）之%20iTerm/)
-- iterm2恢复方法是将备份的json文件存入iterm恢复目录, 再次打开iterm会自动导入该配置文件, 但是配置目录并不是这里. 所以导入完成后, 就可以删除这个文件了(目录见配置文件)
-- 修改如下
-    - 增加 hotkey window: 下拉终端, 快捷键 cmd+f2. 类似 linux-guake。iterm2 官方现在支持hotkey win了，无需设置
-    - 添加 option+左右键 以单词为单位移动
-
-关于 iterm2 下，中文字体间隔和英文间隔不适配的问题
-1. Profile->Text 启用 use diffenert for non-ASCII
-2. 调整 non-ASCII 字体大小，一般比 ASCII 字体大两个字号即可。
-
-备份：iterm2 配置文件为 `~/Library/Preferences/com.googlecode.iterm2.plist`，但不支持直接软连接此文件，原因不知。
-1. 将 plist 硬连接到自己的云端备份位置
-2. 需要恢复时，将配置文件移动到 `~/Library/Application Support/iTerm2/DynamicProfiles`
-
-也可以使用 iterm2 的 defaults 命令导入导出配置文件，不过这样不能随时更新到云端，所以我放弃了这个方法。
-
-#### 设置快捷键启动
+### 设置快捷键启动
 - 创建 自动操作脚本, 里面可选shell命令. 完成后移动到 `~/Library/Services` 目录即可在 设置->键盘->快捷键->服务->通用 下找到该选项, 设置快捷键即可
 - 创建shell命令如下
     ```Bash
     open /Applications/iTerm.app/
     exit 0
     ```
-
-osx 不同的是，osx可以通过时间机器迁移/恢复整个系统，不同机器间也可以。
-
-link:https://developer.apple.com/library/archive/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/FileSystemOverview/FileSystemOverview.html[apple file system basic]
-
--- 1-codes/setups/osx/config.sh
+++ 1-codes/setups/osx/config.sh
@@ -7,90 +7,45 @@ readonly NOT_INSTALL="not_install"
 readonly WORK_DIR=`dirname $0`
 
 function main(){
+    # osx 上超级好用的包管理器
     if [ $(isInstall brew) == NOT_INSTALL ];then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        install_brew
     fi
-
-    # 阿里源包不齐全，所以还是用ustc的比较合适
-    # 替换 Homebrew
-    git -C "$(brew --repo)" remote set-url origin https://mirrors.ustc.edu.cn/brew.git
-    # 替换 Homebrew Core
-    git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
-    # 替换 Homebrew Cask
-    git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask.git
-    brew update
-    echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles' >> ~/.zprofile
-    source ~/.zprofile
 
     brew install git tree wget curl mycli
 
+    install_go
+
+    # osx 上超级好用的终端
+    brew install --cask iterm2
+
+    # 文本编辑器 vscode， 可按需更换为自己喜欢的编辑器。
+    brew install --cask visual-studio-code
+
+    # god use vpn
+    # brew install shadowsocks-libev
+    # brew services start shadowsocks-libev
+
     if [ $(isInstall zsh) == NOT_INSTALL ];then
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        brew install zsh
+        # oh my zsh
+        git clone https://mirrors.tuna.tsinghua.edu.cn/git/ohmyzsh.git
+        pushd ohmyzsh/tools
+        REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/ohmyzsh.git sh install.sh
+        popd
     fi
 
-    brew cask install iterm2
-    brew cask install visual-studio-code
+    # spacevim 超级好用的 vim 扩展
+    install_spacevim
 
-    brew install shadowsocks-libev
-    brew services start shadowsocks-libev
+    # echo "安装pip && 配置豆瓣源"
+    # install_pip
 
-    # spacevim:
-    curl -sLf https://spacevim.org/install.sh | bash
-
-    # oh my zsh
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-    brew tap homebrew/cask-fonts
-    # spacevim 图标字体
-    brew cask install font-hack-nerd-font
-
-    echo "安装pip && 配置豆瓣源"
-    install_pip
-
-    echo "安装Shadowsocks"
-    install_shadowsock
-
-    # 输入法自动切换命令行插件。vim切换需要此插件。
-    curl -Ls https://raw.githubusercontent.com/daipeihust/im-select/master/install_mac.sh | sh
-    
-
-    echo "安装Go, 版本 1.11.2"
-    wget -c https://dl.google.com/go/go1.11.2.darwin-amd64.tar.gz -O go.tar.gz
-    tar -xzf go.tar.gz && mv go /usr/local/Cellar/ && ln -s /usr/local/Cellar/go/bin/go /usr/local/bin/go
-
-    local caskInstall=`brew cask list`
-
-    # 判断是否安装 -z string长度为0且为真， 参考 http://docs.linuxtone.org/ebooks/C&CPP/c/ch31s05.html
-    if [ -z $(echo $caskInstall | grep -wo iterm2) ];then
-        # 安装 iterm2, 后续导入 ./iterm2.json 配置文件即可还原配置
-        brew cask install iterm2
-    fi
-
-    brew install go
-    # 设置goroot。brew安装的软件包在 /usr/local/Cellar/go/{version}/libexec，不清楚为什么这么搞。
-    # 不应该放在 /usr/local/src 下么，而且应该设置个软连接，然后其他的地方都用软连接么？这么着版本一变不全都乱套了吗？
-    # export GOROOT='/usr/local/Cellar/go/{version}/libexec'
-    # 安装必要软件包。简单的方法是打开vscode，会自动推荐你安装那些软件
-    go get github.com/mdempsky/gocode
-    go get github.com/uudashr/gopkgs/v2/cmd/gopkgs
-    go get github.com/ramya-rao-a/go-outline
-    go get github.com/rogpeppe/godef
-    go get github.com/sqs/goreturns
-    go get golang.org/x/lint/golint
-    go get golang.org/x/tools/gopls
-    go get golang.org/x/tools/cmd/goimports
-
-    brew install protobuf
-    go get -u github.com/golang/protobuf/protoc-gen-go
+    # local caskInstall=`brew list --cask`
 
     git config --global user.name "wzs"
     git config --global user.email "zhensheng.five@gmail.com"
     git config --global url."git@github.com:".insteadOf "https://github.com/"
-
-    # brew cask install alfred
-    brew install npm
-    npm config set registry https://registry.npm.taobao.org
-    npm install -g @vue/cli
 }
 
 # 判断是否安装
@@ -100,6 +55,31 @@ function isInstall(){
     else
         echo NOT_INSTALL
     fi
+}
+
+# 从中科大源安装, 参考 https://mirrors.tuna.tsinghua.edu.cn/help/homebrew/
+function install_brew(){
+    git clone --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/install.git brew-install
+    /bin/bash brew-install/install.sh
+    rm -rf brew-install
+
+    echo 'export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"' >> ~/.zprofile
+    echo 'export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"' >> ~/.zprofile
+    source ~/.zprofile
+    brew update
+}
+
+function install_go(){
+    brew install go
+
+    brew install protobuf
+    go get -u github.com/golang/protobuf/protoc-gen-go
+}
+
+function install_spacevim(){
+    curl -sLf https://spacevim.org/install.sh | bash
+    # 输入法自动切换命令行插件。vim切换需要此插件。
+    brew install fselect im-select
 }
 
 function install_pip(){
